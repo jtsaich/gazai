021f7f8a00286a963918634630e51d8f31878f0a
Jack Tsai <jtsai0606@gmail.com>
2024-03-15 08:24:05 +0800

    Demo front page explore

---
+++ app/(protected)/_components/infinite-scroll-card.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef } from 'react';
+
+export default function InfiniteScrollCard({
+  creditUrl,
+  imgAlt = 'placeholder',
+  imgSrc = '/placeholder.jpg',
+  shotBy,
+  newLimit,
+  isLast
+}) {
+  /**
+   * Select the Card component with useRef
+   */
+  const cardRef = useRef();
+
+  /**
+   * Implement Intersection Observer to check if the last Card in the array is visible on the screen, then set a new limit
+   */
+  useEffect(() => {
+    if (!cardRef?.current) return;
+
+    const observer = new IntersectionObserver(([entry]) => {
+      if (isLast && entry.isIntersecting) {
+        newLimit();
+        observer.unobserve(entry.target);
+      }
+    });
+
+    observer.observe(cardRef.current);
+  }, [isLast]);
+
+  return (
+    <div className="shadow-lg rounded-xl p-2 w-full bg-white" ref={cardRef}>
+      <a href="#">
+        {/* <a href={creditUrl} target="_blank"> */}
+        <div className="w-full h-96 relative overflow-hidden rounded-xl">
+          <img
+            src={imgSrc}
+            alt={imgAlt}
+            className={`w-full h-full object-cover`}
+          />
+        </div>
+        <div className="rounded-b-xl p-4">
+          Credit:<span className="font-semibold"> {shotBy}</span>
+        </div>
+      </a>
+    </div>
+  );
+}
--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,11 +1,35 @@
+'use client';
+
 import LogoutButton from '@/components/auth/logout-button';
+import InfiniteScrollCard from './_components/infinite-scroll-card';
 import { ArrowLeftOnRectangleIcon } from '@heroicons/react/20/solid';
 
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
-export default async function IndexPage() {
+export default function IndexPage() {
+  const [images, setImages] = useState<any>([]);
+  const [page, setPage] = useState(1);
+
+  const fetchImages = async () => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_UNSPLASH_BASE_URL}?query=anime&page=${page}`,
+      {
+        headers: {
+          Authorization: `Client-ID ${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}`
+        }
+      }
+    );
+    const { results } = await response.json();
+    setImages((prev: any) => [...prev, ...results]);
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
+
   return (
-    <main className="p-10">
+    <main className="p-12 lg:px-20">
       <h1 className="text-4xl font-bold mb-4">Start making art!</h1>
 
       <div className="flex overflow-hidden rounded-md gap-4">
@@ -19,6 +43,19 @@ export default async function IndexPage() {
           <ArrowLeftOnRectangleIcon className="h-4 w-4 mr-2" />
           Logout
         </LogoutButton>
+      </div>
+      <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
+        {images.map((image: any, index: number) => (
+          <InfiniteScrollCard
+            key={image.id}
+            imgSrc={image.urls.regular}
+            imgAlt={image.alt_description}
+            shotBy={image.user.name}
+            creditUrl={image.links.html}
+            isLast={index === images.length - 1}
+            newLimit={() => setPage(page + 1)}
+          />
+        ))}
       </div>
     </main>
   );
